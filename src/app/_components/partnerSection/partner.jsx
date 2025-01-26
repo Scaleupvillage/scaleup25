89372b0c1cffa6540002b45a43e98ec348edb404
Edwinliby <edwinliby30@gmail.com>
2025-01-26 21:34:41 +0530

    added partners logos

--- src/app/_components/partnerSection/partner.jsx
+++ src/app/_components/partnerSection/partner.jsx
@@ -1,19 +1,69 @@
+'use client'
+
+import { useState, useEffect, useRef } from 'react'
 import styles from './partner.module.css';
 import Image from 'next/image';
 import design from '@/../../public/group-1.svg';
-import l2 from '@/../../public/l2.svg';
-import { partner } from './partnerData';
+// import l2 from '@/../../public/l2.svg';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
+
+const PartnerURL = "https://opensheet.elk.sh/1iROX8lbSoKx0riimNSjAzbBT64zGBvWN9P3eDApcetw/Sheet1"
 
 export default function Partner() {
+    const [partners, setPartners] = useState([]);
+    const listRef = useRef(null);
+
+    useEffect(() => {
+        fetch(PartnerURL)
+            .then((response) => response.json())
+            .then((data) => {
+                const updatedPartners = data
+                    .filter((partner) => partner.Pic)
+                    .map((partner) => {
+                        const driveId = partner.Pic.split('/')[5];
+                        const imageUrl = `https://drive.google.com/uc?id=${driveId}`;
+                        return {
+                            imageUrl,
+                        };
+                    });
+                setPartners(updatedPartners);
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
+
+    useEffect(() => {
+        if (partners.length > 0) {
+            gsap.fromTo(
+                listRef.current.querySelectorAll(`.${styles.card}`),
+                { opacity: 0, scale: 0.5 },
+                {
+                    opacity: 1,
+                    scale: 1,
+                    stagger: 0.2,
+                    duration: 1,
+                    ease: "power2.out",
+                    scrollTrigger: {
+                        trigger: listRef.current,
+                        start: "top 80%",
+                        toggleActions: "play none none none",
+                    },
+                }
+            );
+        }
+    }, [partners]);
+
     return (
         <div className={styles.partner}>
-            <Image
+            {/* <Image
                 src={l2}
                 alt="design"
                 width={400}
                 height={400}
                 className={styles.l2}
-            />
+            /> */}
             <div className={styles.head}>
                 <h1>ScaleUp Partners</h1>
                 <Image
@@ -24,24 +74,19 @@ export default function Partner() {
                 />
             </div>
 
-            <div className={styles.list}>
-                {Object.entries(partner).map(([tier, partners]) => (
-                    <div key={tier} className={styles.tierSection}>
-                        <h2 className={styles.tierTitle}>{tier} Partners</h2>
-                        <div className={styles.cardContainer}>
-                            {partners.map((partner, index) => (
-                                <div className={styles.card} key={index}>
-                                    <Image
-                                        src={partner.img}
-                                        alt={`${tier} partner image`}
-                                        width={1000}
-                                        height={1000}
-                                    />
-                                </div>
-                            ))}
+            <div className={styles.list} ref={listRef}>
+                {
+                    partners.map((partner, index) => (
+                        <div className={styles.card} key={index}>
+                            <Image
+                                src={partner.imageUrl}
+                                alt={`Partner ${index + 1}`}
+                                width={200}
+                                height={200}
+                            />
                         </div>
-                    </div>
-                ))}
+                    ))
+                }
             </div>
         </div>
     );
