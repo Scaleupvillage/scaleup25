67c978dadf04f861b42aef0da2a7443a6d148bc4
Edwinliby <edwinliby30@gmail.com>
2025-01-28 10:45:10 +0530

    added partners

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default async function Home() {
             <Events />
             <Pre />
             {/* <Schedule /> */}
-            {/* <Partner /> */}
+            <Partner />
             <Faq />
         </>
     );
